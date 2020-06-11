ce82079b4fca9276788e667ceecfa4c96755cf71
ashish003 <aashish@lutron.com>
2020-06-11 23:33:14 +0530

    Completed

--- 03-Class-Component/src/SeasonDisplay.js
+++ 03-Class-Component/src/SeasonDisplay.js
@@ -1,4 +1,17 @@
+import './SeasonDisplay.css';
 import React from 'react';
+import Spinner from './Spinner';
+
+const seasonConfig = {
+    summer: {
+        text: "Let's hit the beach",
+        iconName: 'sun'
+    },
+    winter: {
+        text: 'Burr, it is chilly',
+        iconName: 'snowflake'
+    }
+}
 
 const getSeason = (lat, month) =>
 {
@@ -12,15 +25,18 @@ const getSeason = (lat, month) =>
 const SeasonDisplay = props =>
 {
     const season = getSeason(props.lat, new Date().getMonth());
-    const text = season === 'winter' ? 'Burr, it is chilly' : 'Lets hit the beach';
-    const icon = season === 'winter' ? 'snowflake' : 'sun';
+    const { text, iconName } = seasonConfig[season]
     return (
-        <div>
-            <i className={`${icon} icon`} />
+        <div className={`season-display ${season}`}>
+            <i className={` icon-left massive ${iconName} icon`} />
             <h1>{text}</h1>
-            <i className={`${icon} icon`} />
+            <i className={` icon-right massive ${iconName} icon`} />
         </div>
     );
+};
+
+Spinner.defaultProps = {
+    message: 'Loading...'
 };
 
 export default SeasonDisplay;
--- 03-Class-Component/src/index.js
+++ 03-Class-Component/src/index.js
@@ -1,11 +1,12 @@
 import React from "react";
 import ReactDOM from 'react-dom'
 import SeasonDisplay from './SeasonDisplay'
+import Spinner from './Spinner'
 
 class App extends React.Component
 {
     state = { lat: null, errorMessage: '' };
-
+    
     componentDidMount()
     {
         window.navigator.geolocation.getCurrentPosition(
@@ -14,7 +15,7 @@ class App extends React.Component
         );
     }
 
-    render()
+    renderContent()
     {
         if (this.state.errorMessage && !this.state.lat) {
             return <div>Error: {this.state.errorMessage}</div>
@@ -24,8 +25,12 @@ class App extends React.Component
             return <SeasonDisplay lat={this.state.lat} />
         }
 
-        return <div>Loading!</div>
-        
+        return <Spinner message='Please accept location request'/>;
+    }
+
+    render()
+    {
+        return <div className='border red'>{this.renderContent()}</div>;
     }
 }
 
